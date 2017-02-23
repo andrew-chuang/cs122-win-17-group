1226948c7ae73bbffd80b8f15fe35a009f373a7e
Andrew Chuang <chuangspokane@gmail.com>
2017-02-22 20:44:18 -0600

    Finished first portion of Django Tutorial
    Trying to finish Django Tutorial part 1

---
+++ yelp_rec_django/polls/urls.py
@@ -0,0 +1,13 @@
+#
+#
+#
+#
+#
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
--- yelp_rec_django/polls/views.py
+++ yelp_rec_django/polls/views.py
@@ -1,3 +1,14 @@
+# CMSC 122 Winter 2017 Arif-Chuang-Hori-Teehan
+# UChicago Yelp Recommender Project
+#
+#
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
