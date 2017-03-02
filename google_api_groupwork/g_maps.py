337df43892cbee0491ed1c0249995ff302a6e328
Andrew Chuang <chuangspokane@gmail.com>
2017-03-01 19:56:54 -0600

    Working on Google Maps file.
    For some reason, the Google Maps module doesn’t work in the VM but
    works in my laptop. I am trying to write the functions out and then go
    to office hours and fix the problem with the VM? Also made changes to
    Django files as I am working through the tutorial.

--- google_api_groupwork/g_maps.py
+++ google_api_groupwork/g_maps.py
@@ -5,59 +5,22 @@
 # 
 #
 
-from google maps import convert, directions, timezone, places
-import httplib2
+import googlemaps 
+from datetime import datetime
 
-                     ###############################
-                     #   Following function        #
-                     # written by Google Maps API  #
-                     # NOT a product of the group  #
-                     ###############################
+API_KEY = 'AIzaSyCHgCLQKPNQDVJvycSL0kRh1AdTVYTwm9Q'
 
-# Location for below function needs to be in coordinate form. 
+from googlemaps import GoogleMaps
+gmaps = GoogleMaps(API_KEY)
 
-def places(client, query, location=None, radius=None, language=None,
-           min_price=None, max_price=None, open_now=False, type=None,
-           page_token=None):
-    """
-    Places search.
-    :param query: The text string on which to search, for example: "restaurant".
-    :type query: string
-    :param location: The latitude/longitude value for which you wish to obtain the
-        closest, human-readable address.
-    :type location: string, dict, list, or tuple
-    :param radius: Distance in meters within which to bias results.
-    :type radius: int
-    :param language: The language in which to return results.
-    :type langauge: string
-    :param min_price: Restricts results to only those places with no less than
-        this price level. Valid values are in the range from 0 (most affordable)
-        to 4 (most expensive).
-    :type min_price: int
-    :param max_price: Restricts results to only those places with no greater
-        than this price level. Valid values are in the range from 0 (most
-        affordable) to 4 (most expensive).
-    :type max_price: int
-    :param open_now: Return only those places that are open for business at
-        the time the query is sent.
-    :type open_now: bool
-    :param type: Restricts the results to places matching the specified type.
-        The full list of supported types is available here:
-        https://developers.google.com/places/supported_types
-    :type type: string
-    :param page_token: Token from a previous search that when provided will
-        returns the next page of results for the same search.
-    :type page_token: string
-    :rtype: result dict with the following keys:
-        results: list of places
-        html_attributions: set of attributions which must be displayed
-        next_page_token: token for retrieving the next page of results
-    """
-    return _places(client, "text", query=query, location=location,
-                   radius=radius, language=language, min_price=min_price,
-                   max_price=max_price, open_now=open_now, type=type,
-                   page_token=page_token)
-
-
-
+address = 'Constitution Ave NW & 10th St NW, Washington, DC'
+lat, lng = gmaps.address_to_latlng(address)
+print lat, lng
+"""
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+print(directions_result)
+"""
 
