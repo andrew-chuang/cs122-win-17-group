a4a0e9fd9993598b64de81fbdf6365d62a3d0b60
Andrew Chuang <chuangspokane@gmail.com>
2017-03-04 15:47:34 -0600

    Django Tutorial 4/5
    Finished with everything except the Testing

--- yelp_rec_django/polls/models.py
+++ yelp_rec_django/polls/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import datetime
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 
