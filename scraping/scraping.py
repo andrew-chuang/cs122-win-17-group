f35dd1bf5cd5ae0b0a51c959f78f08ddd2bdc8b2
Salman <sarif6@uchicago.edu>
2017-03-04 13:45:38 -0600

    scrapin

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -5,7 +5,7 @@ import re
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 
-MAX_REVIEWS = 500
+MAX_REVIEWS = 50
 
 pm = urllib3.PoolManager()
 
@@ -15,9 +15,7 @@ with open('config_secret.json') as cred:
     auth = Oauth1Authenticator(**creds)
     client = Client(auth)
 
-client = Client(auth)
-
-
+# client = Client(auth)
 
 class business:
 
@@ -48,44 +46,6 @@ def find_intended_restaurant(name, loc):
 	return results
 
 
-def make_url(biz_id=None, user_id=None):
-	'''
-	Constructs the URL for either a business page or a user's page.
-
-	Inputs:
-		EITHER biz_id or user_id, strings
-	'''
-	if biz_id:
-		url = 'https://www.yelp.com/biz/{}?'.format(biz_id)
-
-	elif user_id:
-		url = 'https://www.yelp.com/user_details_reviews_self?'\
-		'userid={}&rec_pagestart=0'.format(user_id)
-
-	return url
-
-
-def scrape_biz_basics(biz_id):
-	'''
-	Given a business ID, scrapes basic information. 
-	'''
-	biz_url = make_url(biz_id = biz_id)
-
-	html = pm.urlopen(url=biz_url, method="GET").data
-	soup = bs4.BeautifulSoup(html, "html.parser")
-
-	address = soup.find_all('address')[1].text.strip()
-
-	aggregate_info = soup.find_all('div', itemprop='aggregateRating')[0]
-
-	review_count = int(aggregate_info.text.strip())
-	
-	agg_rating = aggregate_info.find('meta')['content']
-	agg_rating = float(agg_rating)
-
-
-	return (address, review_count, agg_rating)
-
 
 def scrape_biz_reviews(biz_id):
 	'''
@@ -94,16 +54,18 @@ def scrape_biz_reviews(biz_id):
 
 	Returns: List of dictionaries, each dictionary containing information
 		for one review. 
+
+	Example usage: reviews = scrape_biz_reviews('medici-on-57th-chicago')
 	'''
 	review_list = []
 
 	biz = business(biz_id)
 
-
 	if biz.review_count <= MAX_REVIEWS:
 		pages = range(0, biz.review_count, 20)
 	else:
 		pages = range(0, MAX_REVIEWS, 20)
+
 
 	for page in pages:
 		url = biz.url + 'start={}'.format(page)
@@ -112,11 +74,32 @@ def scrape_biz_reviews(biz_id):
 		soup = bs4.BeautifulSoup(html, "html.parser")
 
 		# Only scraping English reviews
+		'''
 		reviews = soup.find_all('p', lang='en')
 		review_dates = soup.find_all('span', class_="rating-qualifier")
+		'''
+
+		rev_data = soup.find_all('div', itemprop='review')
+		users = soup.find_all('div', class_='review review--with-sidebar')
 
 		date_list = []
-		for date in review_dates:
+
+		for i in range(0, len(rev_data)):
+			review_dict = {}
+
+			review_dict['text'] = rev_data[i].find('p', \
+				itemprop='description').text.strip()
+
+			review_dict['date'] = rev_data[i].find('meta', \
+				itemprop='datePublished')['content']
+
+			stars = rev_data[i].find('meta', itemprop='ratingValue')['content']
+			review_dict['stars'] = float(stars)
+
+			review_dict['user_id'] = users[i]['data-signup-object'].split(':')[1]
+
+			review_list.append(review_dict)
+			'''
 			if len(date.attrs['class']) > 1:
 				continue 
 
@@ -143,7 +126,7 @@ def scrape_biz_reviews(biz_id):
 			review_dict['date'] = date_list[i]
 
 			review_list.append(review_dict)
-
+		'''
 
 	return review_list
 
@@ -166,4 +149,40 @@ def scrape_biz_attributes():
 	
 
 
+def scrape_biz_basics(biz_id):
+	'''
+	Given a business ID, scrapes basic information. 
+	'''
+	biz_url = make_url(biz_id = biz_id)
+
+	html = pm.urlopen(url=biz_url, method="GET").data
+	soup = bs4.BeautifulSoup(html, "html.parser")
+
+	address = soup.find_all('address')[1].text.strip()
+
+	aggregate_info = soup.find_all('div', itemprop='aggregateRating')[0]
+
+	review_count = int(aggregate_info.text.strip())
+	
+	agg_rating = aggregate_info.find('meta')['content']
+	agg_rating = float(agg_rating)
+
+
+	return (address, review_count, agg_rating)
+
+def make_url(biz_id=None, user_id=None):
+	'''
+	Constructs the URL for either a business page or a user's page.
+
+	Inputs:
+		EITHER biz_id or user_id, strings
+	'''
+	if biz_id:
+		url = 'https://www.yelp.com/biz/{}?'.format(biz_id)
+
+	elif user_id:
+		url = 'https://www.yelp.com/user_details_reviews_self?'\
+		'userid={}&rec_pagestart=0'.format(user_id)
+
+	return url
 
