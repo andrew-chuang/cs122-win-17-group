146b6c3d62a26db0104d2eef9b98c57a1fa51ee1
Andrew Chuang <chuangspokane@gmail.com>
2017-03-05 15:45:06 -0600

    Model for User Input regarding restaurants completed.
    Have to work on manipulated the data.

--- Project/yelp_rec_django/polls/views.py
+++ Project/yelp_rec_django/polls/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import redirect
 
 from .models import Choice, Question
 
