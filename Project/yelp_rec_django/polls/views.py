1862e9e07240eb157b43dcc05b8ff648e5d0c718
Andrew Chuang <chuangspokane@gmail.com>
2017-03-05 12:37:15 -0600

    Django mods
    Finished the site tutorial, working on forms

--- Project/yelp_rec_django/polls/views.py
+++ Project/yelp_rec_django/polls/views.py
@@ -18,21 +18,14 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    model = Question
+    template_name = 'polls/detail.html'
+
 
 
 class ResultsView(generic.DetailView):
