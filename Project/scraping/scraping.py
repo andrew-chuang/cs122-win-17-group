7ea66d00b2f0e2acd31894410d89e1610a421e49
Salman <sarif6@uchicago.edu>
2017-03-05 20:27:01 -0600

    finished docstrings

--- Project/scraping/scraping.py
+++ Project/scraping/scraping.py
@@ -10,7 +10,7 @@ MAX_USER_REV = 15
 
 # Supress warning output from urllib3
 urllib3.disable_warnings()
-# Set number of connections in order to use threading
+# Set number of connections in order to use threading/pooling
 pm = urllib3.PoolManager(num_pools=5, maxsize=10)
 
 # read API keys for Yelp
@@ -106,7 +106,7 @@ def scrape_biz_reviews(business_id):
 			for one review. Includes: user ID, date, stars, text. 
 		3: Set containing all of the user IDs and their review counts
 			corresponding to the scraped reviews. 
-	Example usage: reviews = scrape_biz_reviews('medici-on-57th-chicago')
+	Example: reviews = scrape_biz_reviews('medici-on-57th-chicago')
 	'''
 	review_list = []
 	user_set = set()
@@ -165,27 +165,27 @@ def scrape_biz_reviews(business_id):
 def scrape_user_reviews(user_id, count):
 	'''
 	Given a user ID, scrapes up to MAX_USER_REV of the user's reviews. 
-	Only scrapes reviews with 4 or 5 stars. Uses threading to save time.
+	Only scrapes reviews with 4 or 5 stars. Uses multiprocessing to save time.
 
 	Inputs:
 		user_id: string, unique to each Yelp user
 		count: how many reviews that user has made. This is found in
 			scrape_biz_reviews. 
-	
+
 	Returns: List of dictionaries, each dictionary containing information 
 		for one review. Includes: user ID, business ID, stars, text. 
 
 	Example: usr_rev = scrape_user_reviews('29buG-NLQkLHwz8B2Newcw', 338)
 	'''
 	review_list = []
-
 	url = 'https://www.yelp.com/user_details_reviews_self?'\
 	'userid={}&review_sort=rating&rec_pagestart={}'
 
 	pages = range(0, count, 10)
 
+	# Create a list of lists of 5 URLs to use for multiprocessing. 
 	urls = [url.format(user_id, i) for i in pages]
-	urls = [urls[i:i + 5] for i in range(0, len(urls), 5)]
+	urls = [urls[i:i+5] for i in range(0, len(urls), 5)]
 
 	for url_set in urls:
 		soups = ThreadPool(5).imap(fetch_soup, url_set)
@@ -195,7 +195,6 @@ def scrape_user_reviews(user_id, count):
 
 			print('==============================')
 			for i in range(1, len(rev_data), 2):
-
 				review_dict = {}
 
 				stars = float(rev_data[i].find_all('div', 
@@ -217,9 +216,10 @@ def scrape_user_reviews(user_id, count):
 						
 						review_list.append(review_dict)
 						print(review_dict['business_id'], review_dict['stars'])	
-					if len(review_list) >= MAX_USER_REV:
-						return review_list
-		return review_list
+					
+						if len(review_list) >= MAX_USER_REV:
+							return review_list
+	return review_list
 
 
 def fetch_soup(url):
@@ -230,6 +230,7 @@ def fetch_soup(url):
 	html = pm.urlopen(url=url, method='GET').data
 	soup = bs4.BeautifulSoup(html, "html.parser")
 	return soup
+
 
 
 ##########################################################################
@@ -261,6 +262,7 @@ def scrape_biz_basics(business_id):
 
 
 	return (address, review_count, agg_rating)
+
 
 def make_url(business_id=None, user_id=None):
 	'''
