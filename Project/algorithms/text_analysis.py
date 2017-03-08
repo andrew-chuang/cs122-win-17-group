1cc3d60d034d41f422216a438d727faadd666c64
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 22:10:25 -0600

    f

--- Project/algorithms/text_analysis.py
+++ Project/algorithms/text_analysis.py
@@ -207,8 +207,8 @@ def combine_scores(overlap_score, sim_score, sent_score):
     score_frame.columns = ['id', 'sims', 'sents', 'overlaps']
 
     factor = len(score_frame.overlaps)
-    score_frame = pd.concat([score_frame, (factor // 3) * (.5 * score_frame.sents + score_frame.sims) + \
-        2 * (factor // 3) * score_frame.overlaps], 1)
+    score_frame = pd.concat([score_frame, 2 * (factor // 3) * (.5 * score_frame.sents + score_frame.sims) + \
+        (factor // 3) * score_frame.overlaps], 1)
 
     
     score_frame.columns = ['id','similarity', 'sentiment', 'overlaps', 'sums']
