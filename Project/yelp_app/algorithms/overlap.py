9ac7736d63f0475b411e9a0a3372fafb7f9269f9
Anne Rogers <amr@cs.uchicago.edu>
2017-03-14 11:59:07 -0500

    final revision

--- Project/yelp_app/algorithms/overlap.py
+++ Project/yelp_app/algorithms/overlap.py
@@ -1,22 +1,21 @@
-import sqlite3
-import pandas as pd
-import json
-import operator as op
-import numpy as np
-import scipy as sp
 from math import sqrt, erf, log
 
-# include function for sql calls
-# include function that sends sqlcall to pandas database
-# pandas db w/ column for each user where row is for a different rest.
 
-# weighting functions to try out
+# weighting function
 
 
 #lognormal cdf function
 #ended up not using because it makes all values the same 
 def weighting(x, u, s):
     '''
+    Weights the values
+
+    inputs:
+        x - float
+        u - mean, float
+        s - std deviation, float
+    returns:
+        float
     '''
     return  (1 / 2) * (1 + erf((x - u)/(log(s) * sqrt(2))))
 
@@ -24,6 +23,14 @@ def weighting(x, u, s):
 
 def count_intersections(user_reviews):
     '''
+    Counts the number of times a restaurant appears in the user_reviews
+    and normalizes it
+
+    inputs:
+        DataFrame
+
+    returns:
+        dictionary
     '''
 
     count_dict = {}
@@ -33,10 +40,9 @@ def count_intersections(user_reviews):
         else:
             count_dict[i_d] += 1
 
-    # normalizes the scores so that they add to 1
+    # normalizes the scores 
     for key in count_dict:
         count_dict[key] = count_dict[key] / len(user_reviews["business_id"])
-        #count_dict[key] = weighting(count_dict[key], 0 , .125)
     
     
     return count_dict
